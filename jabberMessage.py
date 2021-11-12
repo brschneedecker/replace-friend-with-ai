7a18bcb24c3ba4976521e5945590370d9ba6d8a7
Ben Schneedecker <33423019+brschneedecker@users.noreply.github.com>
2021-11-11 21:15:08 -0500

    feat: add language model and basic HTML parsing

--- jabberMessage.py
+++ jabberMessage.py
@@ -1,6 +1,8 @@
 # Jabber message class and associated functions
 
 import datetime
+from bs4 import BeautifulSoup
+import glob
 
 class JabberMessage:
 
@@ -12,6 +14,38 @@ class JabberMessage:
 
 	def __str__(self):
 		return "{} ({}): {}".format(self.msg_sender, self.timestamp, self.msg_text)
+
+	def get_msg_text(self):
+		return self.msg_text
+
+def parse_html_file(filepath:str):
+	"""
+	Given and HTML file, parse contents and return a list of message objects
+	"""
+
+	html_file = open(filepath, "r")
+	index = html_file.read()
+
+	soup = BeautifulSoup(index, 'html.parser')
+
+	for message in soup.find_all('div'):
+		message_text = message.text.strip()
+
+		if message_text:
+			yield JabberMessage(None, None, None, message_text)
+
+def parse_all_html_files(directory:str):
+	"""
+	Given a directory, search for all HTML files and parse them, returning a 
+	list of message objects
+	"""
+	messages = []
+
+	for html_file in glob.glob("{}/*.html".format(directory)):
+		for message in parse_html_file(html_file):
+			messages.append(message)
+
+	return messages
 
 if __name__ == "__main__":
 	current_time = datetime.datetime.now()
---
+++ lm.py
@@ -0,0 +1,134 @@
+############################################################
+# Use Jabber messages to train a language model
+############################################################
+
+# Include your imports here, if any are used.
+import string
+import random
+import math
+import jabberMessage
+
+
+def tokenize(text):
+    text = text.strip()
+    tokens = []
+    punctuation = set(string.punctuation)
+    split_chars = punctuation.union(set(string.whitespace))
+    temp_token_chars = []
+
+    for c in text:
+
+        if c in split_chars:
+            if temp_token_chars:
+                temp_token = "".join(temp_token_chars)
+                tokens.append(temp_token)
+
+            if c in punctuation:
+                tokens.append(c)
+
+            temp_token_chars = []
+        else:
+            temp_token_chars.append(c)
+
+
+    if temp_token_chars:
+        temp_token = "".join(temp_token_chars)
+        tokens.append(temp_token)
+
+    return tokens
+        
+
+def ngrams(n, tokens):
+    start = ["<START>" for i in range(n-1)]
+
+    ngrams_list = []
+
+    for token in tokens + ["<END>"]:
+        ngram = (tuple(start), token)
+        ngrams_list.append(ngram)
+
+        if n > 1:
+            start = start[1:]
+            start.append(token)
+
+    return ngrams_list
+        
+
+class NgramModel(object):
+
+    def __init__(self, n):
+        self.n = n
+        self.ngrams_tracker = dict()
+
+    def update(self, sentence):
+        tokens = tokenize(sentence)
+        ngrams_list = ngrams(self.n, tokens)
+
+        for ngram in ngrams_list:
+            context = ngram[0]
+            token = ngram[1]
+
+            if context not in self.ngrams_tracker:
+                self.ngrams_tracker[context] = [token]
+            else:
+                self.ngrams_tracker[context].append(token)
+
+    def prob(self, context, token):
+        tokens_for_context = self.ngrams_tracker[context]
+        total = len(tokens_for_context)
+
+        count = 0
+
+        for item in tokens_for_context:
+            if token == item:
+                count += 1
+
+        return count/total
+
+    def random_token(self, context):
+
+        r = random.random()
+
+        tokens_for_context = set(self.ngrams_tracker[context])
+
+        tokens_for_context = sorted(list(tokens_for_context))
+
+        total_prob = 0
+
+        for token in tokens_for_context:
+            token_prob = self.prob(context, token)
+            total_prob += token_prob
+            if total_prob > r:
+                return token
+
+    def reset_context(self):
+        return ["<START>" for i in range(self.n-1)]
+
+    def random_text(self, token_count):
+        context = self.reset_context()
+
+        tokens = []
+
+        for i in range(token_count):
+            new_token = self.random_token(tuple(context))
+            tokens.append(new_token)
+
+            if new_token == "<END>":
+                context = self.reset_context()
+            else:
+                if self.n > 1:
+                    context = context[1:]
+                    context.append(new_token)
+
+        return " ".join(tokens)
+
+def create_ngram_model(n, path):
+
+    m = NgramModel(n)
+
+    messages = jabberMessage.parse_all_html_files(path)
+
+    for message in messages:
+        m.update(message.get_msg_text())
+
+    return m
